2f51bae17069ff42cefa9f88134eef3406b68b10
neangnavin004 <angkor5383@gmail.com>
2026-03-05 16:25:03 +0700

    fisrt push

---
+++ include/Book.hpp
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#include <string>
+
+class Book {
+private:
+    int id;
+    std::string title;
+    std::string author;
+public:
+    Book(int id, std::string title, std::string author);
+    void display_info() const;
+};
